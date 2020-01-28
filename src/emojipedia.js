7c60474f0d277709a9db558e28edc0dfba8fd374
priyanshitaneja <56130372+priyanshitaneja@users.noreply.github.com>
2020-01-28 23:42:18 +0530

    Add files via upload

---
+++ src/emojipedia.js
@@ -0,0 +1,65 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😻",
+    name: "Smiling Cat Face With Heart-Eyes",
+    meaning:
+      "Overjoyed, laughing cat face with hearts on the eyes. Is very much in love or very grateful for a friendly service. Likes something very much and expresses admiration that way." 
+  },
+  {
+    id: 5,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+    "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits." 
+  },
+
+  {
+    id: 8,
+    emoji: "🍉",
+    name: "Watermelon",
+    meaning:
+    "The juicy fruit stands for hot summer days, stone spitting contests, healthy eating and holidays. The emoticon of the watermelon promises bliss of love. It is also said to have a positive effect on potency." 
+  },
+
+  {
+    id: 6,
+    emoji: "❤️",
+    name: "Red Heart",
+    meaning:
+    "The red heart is the classical symbol of love. Expression of passion and romance. But also in the non-romantic context for friendship and strong bonds."
+  },
+
+  {
+    id: 7,
+    emoji: "🌮",
+    name: "Taco",
+    meaning:
+    "Viva la Mexico! I feel like Mexican food! Popular Mexican fast food dish. Crunchy corn or wheat tortilla filled with minced meat, salad, cheese and salsa."
+  }
+
+
+];
+
+export default emojipedia;
